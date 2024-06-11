a37ae81be44933cc3e279b1413cb5b8fb792ed53
dilanCimarChn <2002dilanchoque@gmail.com>
2024-06-11 05:01:33 -0400

    navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
-import { Tuser } from "./views/tipos-usuario/tipos-usuario";
-import Usuarios from "./views/usuarios/Usuarios";
-import Login from "./views/Login/Login";
-import AsignaciondeEst from "./views/AsignaciondeEst/AsignaciondeEst";
-import Estacionamiento from "./views/Estacionamiento/Estacionamiento";
+// App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Tuser from './views/tipos-usuario/tipos-usuario';
+import Login from './views/Login/Login';
+import Estacionamiento from './views/Estacionamiento/Estacionamiento';
+import AsignaciondeEst from './views/AsignaciondeEst/AsignaciondeEst';
+import Usuarios from './views/usuarios/Usuarios';
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <Tuser></Tuser>
-        
-        
+    <Router>
+      <Routes>
+        <Route path="/" element={<Tuser />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/estacionamiento" element={<Estacionamiento />} />
+        <Route path="/solicitudes" element={<AsignaciondeEst />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/inicio" element={<Estacionamiento />} />
 
-      </div>
-    </>
+
+
+        {/* Asegúrate de agregar aquí cualquier otra ruta que necesites */}
+      </Routes>
+    </Router>
   );
 }
 
