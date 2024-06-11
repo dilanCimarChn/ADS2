b24f52076ec09004d5eb79f0f7c20aaf4d5a8fe3
dilanCimarChn <2002dilanchoque@gmail.com>
2024-06-11 02:38:37 -0400

    asganacionlugarr

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Tuser } from "./views/tipos-usuario/tipos-usuario";
 import Usuarios from "./views/usuarios/Usuarios";
 import Login from "./views/Login/Login";
+import EstadoEstacionamiento from "./components/estado-estacionamiento/estado-esta";
+import UsuariosTable from "./components/estado-estacionamiento/estado-esta";
 import AsignacionLugar from "./components/asigar-lugar/AsignacionLugar";
 
 function App() {
